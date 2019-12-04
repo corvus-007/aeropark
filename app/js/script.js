79c341fca1ce4bdc578202147274961031d0d5e0
Denis <corvus007@yandex.ru>
2019-12-04 15:17:50 +0300

    Обновляет script.js

--- app/js/script.js
+++ app/js/script.js
@@ -2,25 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
   svg4everybody();
 
   $.fancybox.defaults.animationEffect = 'zoom-in-out';
+  $.fancybox.defaults.touch = false;
   $.fancybox.defaults.lang = 'ru';
   $.fancybox.defaults.i18n = {
     ru: {
       CLOSE: 'Закрыть',
       NEXT: 'Следующий',
       PREV: 'Предыдущий',
-      ERROR: 'Запрашиваемое содержимое не смогло загрузиться. <br> Пожалуйста, повторите попытку позже',
+      ERROR:
+        'Cодержимое не смогло загрузиться. <br> Пожалуйста, повторите попытку позже',
       PLAY_START: 'Начать слайдшоу',
       PLAY_STOP: 'Остановить слайдшоу',
       FULL_SCREEN: 'На весь экран',
       THUMBS: 'Миниатрюры',
       DOWNLOAD: 'Скачать',
       SHARE: 'Поделиться',
-      ZOOM: 'Увеличить'
-    }
+      ZOOM: 'Увеличить',
+    },
   };
 
   $('input[type="tel"]').inputmask({
-    mask: '+7 (999) 999-99-99'
+    mask: '+7 (999) 999-99-99',
   });
 
   $.validator.addMethod(
@@ -39,57 +41,55 @@ document.addEventListener('DOMContentLoaded', function() {
     slidesPerView: 'auto',
     loop: true,
     effect: 'coverflow',
+    loopedSlides: 2,
     coverflowEffect: {
       rotate: 0,
       stretch: -50,
       depth: 100,
       modifier: 3,
-      slideShadows: false
+      slideShadows: false,
     },
 
     // Navigation arrows
     navigation: {
       nextEl: '.common-slider-button--next',
-      prevEl: '.common-slider-button--prev'
-    }
+      prevEl: '.common-slider-button--prev',
+    },
   });
 
-  new Swiper(
-    '[data-preview-events-photo-slider]',
-    {
-      speed: 600,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      loop: true,
-      effect: 'coverflow',
-      slideActiveClass: 'swiper-slide-active preview-event--active',
-      coverflowEffect: {
-        rotate: 20,
-        stretch: 0,
-        depth: 350,
-        modifier: 1,
-        slideShadows: true
-      },
-      navigation: {
-        nextEl: '.common-slider-button--next',
-        prevEl: '.common-slider-button--prev'
-      }
-    }
-  );
+  new Swiper('[data-preview-events-photo-slider]', {
+    speed: 600,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    effect: 'coverflow',
+    slideActiveClass: 'swiper-slide-active preview-event--active',
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 350,
+      modifier: 1,
+      slideShadows: true,
+    },
+    navigation: {
+      nextEl: '.common-slider-button--next',
+      prevEl: '.common-slider-button--prev',
+    },
+  });
 
   new Swiper('[data-shop-gallery]', {
     speed: 600,
     navigation: {
       nextEl: '.common-slider-button--next',
-      prevEl: '.common-slider-button--prev'
-    }
+      prevEl: '.common-slider-button--prev',
+    },
   });
 
   new Swiper('[data-content-gallery]', {
     speed: 600,
     navigation: {
       nextEl: '.common-slider-button--next',
-      prevEl: '.common-slider-button--prev'
-    }
+      prevEl: '.common-slider-button--prev',
+    },
   });
 });
