9c5c250ad2affcb9285aaf9e27706da4a76c24f3
Denis <corvus007@yandex.ru>
2019-12-05 17:33:54 +0300

    Форматирует код в webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const webpack = require('webpack'); //to access built-in plugins
 
-// в консоли `NODE_ENV=production gulp assets`
+// в консоли `NODE_ENV=production gulp task_name`
 const isDevelopment =
   // eslint-disable-next-line no-undef
   !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
-
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   output: {
-    filename: 'aeroplan-app.js'
+    filename: 'aeroplan-app.js',
   },
   // plugins: [
   //   new webpack.ProvidePlugin({
