90475895592df621dce72bca4ee434e52340b7d8
Denis <corvus007@yandex.ru>
2020-02-25 09:34:49 +0300

    Обновляет скрипт в movie-card-ambilight.js

--- app/js/modules/movie-card-ambilight.js
+++ app/js/modules/movie-card-ambilight.js
@@ -1,22 +1,26 @@
-window.movieCardEmbilight = (function (window, $) {
+window.movieCardEmbilight = (function() {
   'use strict';
 
   const AMBILIGHT_POSTER_CLASS = 'movie-card__ambilight-poster';
   const movieCards = document.querySelectorAll('.movie-card');
 
+  if (window.matchMedia('(pointer: coarse)').matches) {
+    return;
+  }
+
   if (!movieCards.length) {
     return;
   }
 
-  movieCards.forEach((movieCard) => {
+  movieCards.forEach(movieCard => {
     const poster = movieCard.querySelector('.movie-card__poster');
     const posterImage = poster.querySelector('img');
-    const posterSrc = posterImage.currentSrc || posterImage.src;
+    const posterSrc =
+      posterImage.currentSrc || posterImage.src || posterImage.dataset.src;
     const ambilightElem = document.createElement('div');
 
     ambilightElem.style.backgroundImage = `url(${posterSrc})`;
     ambilightElem.classList.add(AMBILIGHT_POSTER_CLASS);
     poster.insertAdjacentElement('afterend', ambilightElem);
   });
-
-})(window, jQuery);
+})();
